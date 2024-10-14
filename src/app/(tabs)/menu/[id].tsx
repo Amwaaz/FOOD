a2bf2cb912215991abed17fe9f068403d37532cd
unknown <amwaazarshad@gmail.com>
2024-10-14 15:11:40 +0500

    2. Expo router screens

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,16 +1,22 @@
 import { useLocalSearchParams,Stack } from 'expo-router';
-import {View,Text} from 'react-native';
+import {View,Text,Image,StyleSheet} from 'react-native';
+import products from '@/assets/data/products';
 
 
 const ProductDetailScreen= () => {
   const {id} = useLocalSearchParams();
+  const product=products.find((p)=>p.id.toString()===id);
+
+  if(!product){
+    <Text>Product not found!</Text>
+  }
  
     return (
-        <View>
+        <View style={styles.container}>
 {/* changing the screen name */}
           <Stack.Screen 
           options={{
-          title:'DETAILS: ' + id,
+          title:product?.name,
           headerTitleAlign: 'center',
           headerTitleStyle:{
             fontWeight:'bold',
@@ -18,10 +24,40 @@ const ProductDetailScreen= () => {
         }
         }}/>
 
-          <Text>ProductDetailScreen for id : {id}</Text>
+         <Image source={{uri:product?.image}} style={styles.image}/>
+
+         <Text style={{fontSize:22,
+    fontWeight:'bold',}}>Select Size:</Text>
+    
+
+         <Text style={styles.price}>{[product?.price]}</Text>
+         
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+  container:{
+    padding:10,
+  },
+  image: {
+    width: '100%', 
+    aspectRatio:1,   
+    maxHeight:300,
+    resizeMode: 'contain', 
+    marginBottom: 20, 
+  },
+  price:{
+    fontSize:26,
+    fontWeight:'bold',
+    color:"#2A3439",
+    textAlign:'center',
+    marginTop:100,
+  }
+});
+
+
+
 
 export default ProductDetailScreen;
 
@@ -37,3 +73,7 @@ export default ProductDetailScreen;
 //menu tab jo hai usmain do screens aaien gi. the menu and details wali. jb hm details pe jaien ge tb tab neeche se change nhi hoga. so nested navigator. tab navigator k andr stack navigator taake menu aur details main ja sken
 
 //they shouldnt appear as separate pages on the bottom tab footer. jb sirf menu index aur id ko meny folder main move kia tb ye hua k orders  menu/index  menu/id
+
+//This is an arrow function used as the argument for find(). It runs for each element in the products array.
+
+// p: Represents the current product in the products array during each iteration. p.id.toString(): Converts the id of the current product p (which is likely a number) to a string. This is done to ensure it's compared as a string with id.'?' for if the name is null.product?.name. without the product checking "if"
